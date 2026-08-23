ad779adee8b5be99c3825777f7f4fba379c4b8e3
raulvilera <124225576+raulvilera@users.noreply.github.com>
2026-08-23 04:41:25 -0300

    Delete drizzle directory

--- drizzle/0002_numerous_martin_li.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `biology_student_submissions` (
-	`studentId` varchar(64) NOT NULL,
-	`activityId` varchar(64) NOT NULL,
-	`submittedAt` timestamp NOT NULL DEFAULT (now()),
-	CONSTRAINT `biology_student_submissions_studentId` PRIMARY KEY(`studentId`),
-	CONSTRAINT `biology_student_submissions_activityId_unique` UNIQUE(`activityId`)
-);
